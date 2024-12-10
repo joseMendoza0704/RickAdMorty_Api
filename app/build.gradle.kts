e6f1325b58a020404b8ad74fac83b85d48c52c51
joseMendoza0704 <167268205+joseMendoza0704@users.noreply.github.com>
2024-12-09 18:05:10 -0600

    Creacion de carpetas y Librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,19 +51,39 @@ android {
 
 dependencies {
 
+    // Kotlin
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
+
+    // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Image Loading (Coil)
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // Retrofit and Gson for API requests
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // RecyclerView for displaying lists
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Navigation (for navigating between composables)
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+
+    // Testing dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
---
+++ app/src/main/java/com/example/rickandmorty_api/network/ApiClient.kt
@@ -0,0 +1,4 @@
+package com.example.rickandmorty_api.network
+
+class ApiClient {
+}
