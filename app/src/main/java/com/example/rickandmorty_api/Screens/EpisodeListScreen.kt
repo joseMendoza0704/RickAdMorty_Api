fa7c192a028ead0267b608cc87e39342c37fa280
MariaJose1812 <163462704+MariaJose1812@users.noreply.github.com>
2024-12-09 19:08:06 -0600

    Implementación de Lógica de EpisodeListScreen

---
+++ app/src/main/java/com/example/rickandmorty_api/Screens/EpisodeListScreen.kt
@@ -0,0 +1,55 @@
+package com.example.rickandmorty_api.Screens
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.rickandmorty_api.ViewModel.EpisodeListViewModel
+import com.example.rickandmorty_api.ViewModel.EpisodeResult
+import com.example.rickandmorty_api.models.Episode
+
+@Composable
+fun EpisodeListScreen() {
+
+    val viewModel: EpisodeListViewModel = viewModel()
+    val episodesState by viewModel.episodes.collectAsState()
+
+
+    when (episodesState) {
+        is EpisodeResult.Loading -> {
+            CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+        }
+        is EpisodeResult.Error -> {
+            Text("Error: ${(episodesState as EpisodeResult.Error).message}", color = Color.Red, modifier = Modifier.padding(16.dp))
+        }
+        is EpisodeResult.Success -> {
+            val episodes = (episodesState as EpisodeResult.Success<List<Episode>>).data
+            LazyColumn(modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF1e2838)),
+                contentPadding = PaddingValues(16.dp)) {
+                items(episodes) { episode ->
+                    Card(
+                        modifier = Modifier.fillMaxWidth().padding(8.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color(0xFF38761D)),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                    ) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(episode.name, style = MaterialTheme.typography.bodyLarge, color = Color.White)
+                            Text(episode.air_date, style = MaterialTheme.typography.bodySmall, color = Color.White)
+                        }
+                    }
+                }
+            }
+        }
+
+        else -> {}
+    }
+}
